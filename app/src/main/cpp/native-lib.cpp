dc85bd088a45c7f0a8013b250e6f57611ad7d7b9
Matthew Carter <m@ahungry.com>
2020-04-28 15:41:40 -0400

    Hooraay, janet built

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,11 +3,22 @@
 
 // #include "janet.h"
 // #include "janet.c"
+#include "janet.h"
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_ahungry_janet_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "SHello from C++";
-    return env->NewStringUTF(hello.c_str());
+
+  // Eval some janet and do nothing else
+  JanetTable *janet_env;
+  janet_init ();
+  janet_env = janet_core_env (NULL);
+  const char *embed = "((fn [] \"Greetings from janet env\"))";
+  Janet *out = (Janet *) malloc (1);
+  janet_dostring (janet_env, embed, "main", out);
+  janet_deinit ();
+  const char *result = janet_getcstring (out, 0);
+
+  return env->NewStringUTF (result);
 }
