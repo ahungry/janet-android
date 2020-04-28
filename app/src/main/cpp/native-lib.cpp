4e0362571ddbe9fa2eccd7d34da3ba0ff48ff977
Matthew Carter <m@ahungry.com>
2020-04-28 15:29:48 -0400

    Working janet compile perhaps

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,6 +8,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_ahungry_janet_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "SHello from C++";
     return env->NewStringUTF(hello.c_str());
 }
---
+++ app/src/main/cpp/shim.c
@@ -0,0 +1,5 @@
+double
+log2 (double x)
+{
+  return x;
+}
